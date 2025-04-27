e558ebe5d72c461710e5daf20f34fd07c0b9cad2
markjayson545 <markjayson545@gmail.com>
2025-04-27 12:41:58 +0800

    fix: update logger path for consistent log file access and append successful login attempts to sign-in log

--- php/services/logger.php
+++ php/services/logger.php
@@ -1,15 +1,16 @@
 <?php
-$logPath = '/home/mark/apache-xampp-hosting/docutracker/logs/';
+$logPath = __DIR__ . '/../../logs/';
 function writeLog($message, $where)
 {
     try {
         global $logFile, $logPath;
         $logFile = $logPath . $where;
+        $globalLogFile = $logPath . 'global.log';
         $timestamp = date('Y-m-d H:i:s');
         $logMessage = "[$timestamp] $message" . PHP_EOL;
         file_put_contents($logFile, $logMessage, FILE_APPEND);
+        file_put_contents($globalLogFile, $logMessage, FILE_APPEND);
     } catch (\Throwable $th) {
-        // Handle any errors that occur while writing to the log file
         error_log("Failed to write to log file: " . $th->getMessage());
     }
 }
